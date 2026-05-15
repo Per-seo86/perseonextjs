452d72a777fbb4df520c81c3c22d7776a1d5b65f
Lorenzo Curia <camilla.filice@gmail.com>
2026-05-15 13:45:22 +0200

    Fix sitemap index with proper XML sitemapindex format

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://perseo.biz';
+
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${BASE_URL}/pages-sitemap.xml</loc>
+  </sitemap>
+  <sitemap>
+    <loc>${BASE_URL}/posts-sitemap.xml</loc>
+  </sitemap>
+  <sitemap>
+    <loc>${BASE_URL}/categories-sitemap.xml</loc>
+  </sitemap>
+</sitemapindex>`;
+
+  return new Response(xml, {
+    headers: { 'Content-Type': 'application/xml' },
+  });
+}
