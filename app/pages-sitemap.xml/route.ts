4230836ca80108d6b1a0c9069daf6f5aaa6ad217
Per-seo86 <info@perseo.biz>
2026-05-15 14:16:53 +0200

    Remove XSL stylesheet reference from sitemap XML for Google compatibility

--- app/pages-sitemap.xml/route.ts
+++ app/pages-sitemap.xml/route.ts
@@ -29,7 +29,6 @@ export async function GET() {
     .join('');
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
 </urlset>`;
